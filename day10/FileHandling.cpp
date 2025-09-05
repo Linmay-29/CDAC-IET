809b6f46db45a07cd858298e400fb2ed9fb9cd95
KunalPardeshi <kunalpardeshi00281@gmail.com>
2025-09-05 17:03:25 +0530

    update

---
+++ day10/FileHandling.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+//    if(!my_file){
+//     cout << "Error opening the file." << endl;
+//     return 1;
+//    }
+    
+//    string line;
+//    ofstream my_file;
+//    my_file.open("Sample.txt");
+//    my_file << "Heloooooo";
+//    my_file.close();
+//    ifstream my_file1("Sample.txt");
+//    while(getline(my_file1,line)){
+//         cout<<line;
+//    }
+
+    string line;
+    fstream my_file;
+    my_file.open("Sample.txt");
+    my_file << "new World";
+    my_file.seekg(0);
+
+    getline(my_file,line);
+    cout<<line;
+    cout<<endl;
+    cout<<line;
+    my_file.close();
+}
